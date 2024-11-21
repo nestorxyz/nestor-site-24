3a90e968f710363e4b5cc615096d0f5b3268f13d
Nestor Mamani <nmamanipantoja@gmail.com>
2024-11-20 23:08:05 -0500

    Update resume data with new hackathon entries and project descriptions

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -265,51 +265,78 @@ export const DATA = {
   ],
   hackathons: [
     {
-      title: 'Hack Western 5',
-      dates: 'November 23rd - 25th, 2018',
-      location: 'London, Ontario',
+      title: '🧊 Ice',
+      dates: '2023',
+      location: 'Lima, Peru',
       description:
-        'Developed a mobile application which delivered bedtime stories to children using augmented reality.',
+        'Ice is a chrome extension that help you understand wtf you are signing with your wallet 🤘...using AI',
       image:
         'https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-western.png',
-      mlh: 'https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg',
-      links: [],
-    },
-    {
-      title: 'Hack The North',
-      dates: 'September 14th - 16th, 2018',
-      location: 'Waterloo, Ontario',
-      description:
-        'Developed a mobile application which delivers university campus wide events in real time to all students.',
-      image:
-        'https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-the-north.png',
-      mlh: 'https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg',
-      links: [],
-    },
-    {
-      title: 'FirstNet Public Safety Hackathon',
-      dates: 'March 23rd - 24th, 2018',
-      location: 'San Francisco, California',
-      description:
-        'Developed a mobile application which communcicates a victims medical data from inside an ambulance to doctors at hospital.',
-      icon: 'public',
-      image:
-        'https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/firstnet.png',
-      links: [],
-    },
-    {
-      title: 'DeveloperWeek Hackathon',
-      dates: 'February 3rd - 4th, 2018',
-      location: 'San Francisco, California',
-      description:
-        'Developed a web application which aggregates social media data regarding cryptocurrencies and predicts future prices.',
-      image:
-        'https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/developer-week.jpg',
+      mlh: '',
       links: [
         {
           title: 'Github',
           icon: <Icons.github className="h-4 w-4" />,
-          href: 'https://github.com/cryptotrends/cryptotrends',
+          href: 'https://github.com/nestorxyz/ice-signature',
+        },
+      ],
+    },
+    {
+      title: 'Cesio',
+      dates: '2023',
+      location: 'Lima, Peru',
+      description:
+        'Build trustworthy deals: Using custodial payments, contractors have guaranteed payment, and clients can rest assured that payment will only be released once the job is done',
+      image:
+        'https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-the-north.png',
+      mlh: '',
+      links: [
+        {
+          title: 'Site',
+          icon: <Icons.globe className="h-4 w-4" />,
+          href: 'https://cesio-web.vercel.app',
+        },
+      ],
+    },
+    {
+      title: 'Web3Blocks',
+      dates: '2022',
+      location: 'Lima, Peru',
+      description: 'Blog demo site: Find the best Web3 Writers and Blogs',
+      icon: 'public',
+      image:
+        'https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/firstnet.png',
+      links: [
+        {
+          title: 'Site',
+          icon: <Icons.globe className="h-4 w-4" />,
+          href: 'https://web3-blocks.vercel.app',
+        },
+        {
+          title: 'Github',
+          icon: <Icons.github className="h-4 w-4" />,
+          href: 'https://github.com/nestorxyz/web3-blog',
+        },
+      ],
+    },
+    {
+      title: 'Ethereum NFTs Showcase',
+      dates: '2022',
+      location: 'Lima, Peru',
+      description:
+        'Added a feature to showcase NFTs on a link in bio page, using custom web3 API',
+      image:
+        'https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/developer-week.jpg',
+      links: [
+        {
+          title: 'Site',
+          icon: <Icons.globe className="h-4 w-4" />,
+          href: 'https://misfans-add-nft.vercel.app',
+        },
+        {
+          title: 'Github',
+          icon: <Icons.github className="h-4 w-4" />,
+          href: 'https://github.com/nestorxyz/misfans-nft-erc721-metadata/tree/main',
         },
       ],
     },
