e569896590e82dbd45cd8824a2a007fd0b3f395d
Nestor Mamani <nmamanipantoja@gmail.com>
2025-03-26 18:02:15 -0500

    better input

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,11 +1,26 @@
 'use client';
 
 import { useChat } from '@ai-sdk/react';
+import { Button } from '@/components/ui/button';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import { AutoResizeTextarea } from '@/components/ui/autoresize-textarea';
+import { ArrowUpIcon } from 'lucide-react';
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat({
     maxSteps: 3,
   });
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      handleSubmit(e as unknown as React.FormEvent<HTMLFormElement>);
+    }
+  };
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
       <div className="space-y-4">
@@ -16,24 +31,40 @@ export default function Chat() {
               <p>
                 {m.content.length > 0 ? (
                   m.content
-                ) : (
+                ) : m?.parts?.[0].type === 'tool-invocation' ? (
                   <span className="italic font-light">
-                    {'calling tool: ' + m?.toolInvocations?.[0].toolName}
+                    {'calling tool: ' + m?.parts?.[0].toolInvocation.toolName}
                   </span>
-                )}
+                ) : null}
               </p>
             </div>
           </div>
         ))}
       </div>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+      <form
+        onSubmit={handleSubmit}
+        className="border-input bg-background focus-within:ring-ring/10 relative mx-6 mb-6 flex items-center rounded-[16px] border px-3 py-1.5 pr-8 text-sm focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-0"
+      >
+        <AutoResizeTextarea
+          onKeyDown={handleKeyDown}
+          onChange={(e) => handleInputChange(e)}
           value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
+          placeholder="Enter a message"
+          className="placeholder:text-muted-foreground flex-1 bg-transparent focus:outline-none"
         />
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              variant="ghost"
+              size="sm"
+              className="absolute bottom-1 right-1 size-6 rounded-full"
+            >
+              <ArrowUpIcon size={16} className="text-white shrink-0" />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent sideOffset={12}>Submit</TooltipContent>
+        </Tooltip>
       </form>
     </div>
   );
