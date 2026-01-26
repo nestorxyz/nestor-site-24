e6ec72f509e9da891b44d7610421433c1192a4b2
Nestor Mamani <nmamanipantoja@gmail.com>
2026-01-25 22:48:13 -0500

    a

--- src/components/chat/portfolio-header.tsx
+++ src/components/chat/portfolio-header.tsx
@@ -1,10 +1,8 @@
 'use client';
 
-import Image from 'next/image';
-
 export function PortfolioHeader() {
   return (
-    <div className="flex flex-col justify-start pt-12 sm:pt-24 items-start text-left">
+    <div className="flex px-4 lg:px-0 flex-col justify-start pt-12 sm:pt-24 items-start text-left">
       <h1 className="mb-2 text-4xl font-bold tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-emerald-400">
         helloo
       </h1>
